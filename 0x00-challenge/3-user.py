9202f789c5d926da7b2e78a9022d03110c8e6569
Dorothy Dede Quao <128816014+Dedeyd70@users.noreply.github.com>
2023-09-23 15:09:47 +0000

    Update 3-user.py

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -40,7 +40,7 @@ class User():
         if pwd is None or not isinstance(pwd, str):
             self.__password = None
         else:
-            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest()
 
     def is_valid_password(self, pwd):
         """
@@ -82,7 +82,7 @@ if __name__ == '__main__':
 
     user_2.password = 89
     if user_2.password is not None:
-        print("User.password should be None if setter to an integer")
+        print("User.password should be None if set to an integer")
 
     if user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right password")
